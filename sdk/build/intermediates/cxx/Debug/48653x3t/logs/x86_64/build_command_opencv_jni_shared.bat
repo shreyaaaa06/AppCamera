9a45e616cd9be4db579a639630f729f3e4a0b8a6
shreyaaaa@06 <sshreyakp@gmail.com>
2025-09-13 14:03:09 +0530

    Remove build files and native libraries, update gitignore

--- sdk/build/intermediates/cxx/Debug/48653x3t/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Admin\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Admin\\AndroidStudioProjects\\cameraapp\\sdk\\.cxx\\Debug\\48653x3t\\x86_64" ^
-  opencv_jni_shared
